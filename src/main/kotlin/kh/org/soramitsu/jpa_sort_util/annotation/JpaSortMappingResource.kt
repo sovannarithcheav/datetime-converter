d0290fb3ef42bc69f7ce27d1020ca62f1471d57f
CHEAV Sovannarith <cheavsovannarith@gmail.com>
2023-03-13 11:17:22 +0700

    Update JpaSortMappingResource: add getDefaultPropertiesSorting

--- src/main/kotlin/kh/org/soramitsu/jpa_sort_util/annotation/JpaSortMappingResource.kt
+++ src/main/kotlin/kh/org/soramitsu/jpa_sort_util/annotation/JpaSortMappingResource.kt
@@ -1,5 +1,8 @@
 package kh.org.soramitsu.jpa_sort_util.annotation
 
+import kh.org.soramitsu.jpa_sort_util.config.JpaSortArgumentResolverConfig
+import java.lang.reflect.ParameterizedType
+
 interface JpaSortMappingResource<T> {
 
     fun getSource(): MutableMap<String, String>
@@ -7,5 +10,12 @@ interface JpaSortMappingResource<T> {
     fun getInternalSortKeyName(name: String): String? {
         val key = this.getSource()[name]
         return if (key == "_") name else key
+    }
+
+    fun getDefaultPropertiesSorting(): MutableMap<String, String> {
+        val javaName = (this::class.java.genericInterfaces.first() as ParameterizedType).actualTypeArguments.first().typeName
+        return JpaSortArgumentResolverConfig.APP_CONTEXT.classLoader!!.loadClass(javaName).declaredFields.associate {
+            it.name to "_"
+        }.toMutableMap()
     }
 }
